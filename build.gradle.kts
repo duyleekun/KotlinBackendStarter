ef48c7870dc88dc503f7f9bdc3cad9006dede5a9
duyle <duyleekun@gmail.com>
2023-02-08 14:01:38 +0700

    Add basic dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 	id("org.springframework.boot") version "3.0.2"
 	id("io.spring.dependency-management") version "1.1.0"
 	kotlin("jvm") version "1.7.22"
-	kotlin("kapt") version "1.7.20"
+	kotlin("kapt") version "1.7.22"
 	kotlin("plugin.spring") version "1.7.22"
 }
 
@@ -17,13 +17,36 @@ repositories {
 }
 
 dependencies {
+	// Spring framework
 	implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+
+	// Jackson
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+	// R2dbc driver
+	implementation("com.github.jasync-sql:jasync-r2dbc-mysql:2.1.23")
+
+	// R2DBC migrate
+	// https://mvnrepository.com/artifact/name.nkonev.r2dbc-migrate/r2dbc-migrate-spring-boot-starter
+	implementation("name.nkonev.r2dbc-migrate:r2dbc-migrate-spring-boot-starter:2.8.0")
+
+
+	// Kotlin
 	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+
+	//swagger
+	implementation("org.springdoc:springdoc-openapi-webflux-ui:1.6.9")
+	implementation("org.springdoc:springdoc-openapi-kotlin:1.6.9")
+
+
+	// Test
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.projectreactor:reactor-test")
 }
