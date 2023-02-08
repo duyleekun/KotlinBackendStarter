97f508abef8c6fa66b379bf41caead35a1b980d4
duyle <duyleekun@gmail.com>
2023-02-08 10:19:28 +0700

    init with https://start.spring.io/#project-kotlin&language=kotlin&platformVersion=3.0.2&packaging=jar&jvmVersion=17&groupId=vn.ycomm&artifactId=backend_demo&name=backend_demo&description=Demo%20project%20for%20Spring%20Boot&packageName=vn.ycomm.backend_demo&dependencies=data-r2dbc,webflux

---
+++ src/test/kotlin/vn/ycomm/backend_demo/BackendDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package vn.ycomm.backend_demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
